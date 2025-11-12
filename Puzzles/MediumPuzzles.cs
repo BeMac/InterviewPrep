42ed5b81a4b23fa60558035c5e2b5a69f228e07f
Baxter <brianmclaffin@protonmail.ch>
2025-11-11 20:17:51 -0500

    a 10 minute exercise that tripped me up a bit in a live interview.

---
+++ Puzzles/MediumPuzzles.cs
@@ -0,0 +1,48 @@
+﻿
+public class MediumPuzzles
+{
+    
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        string returnPrefix = string.Empty;
+        string firstWord = strs[0];
+        int longestCommonPrefix = 0;
+        for (int outerIndex = 1; strs.Length > outerIndex; outerIndex++)
+        {
+            if (firstWord[0] != strs[outerIndex][0])
+            {
+                return string.Empty;
+            }
+            int wordMatch = 0;
+            for (int innerIndex = 0; strs[outerIndex].Length > innerIndex; innerIndex++)
+            {
+                if (firstWord[innerIndex] == strs[outerIndex][innerIndex])
+                {
+                    wordMatch++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (wordMatch < longestCommonPrefix)
+            {
+                longestCommonPrefix = wordMatch;
+            }
+            else
+            {
+                longestCommonPrefix = wordMatch;
+            }
+        }
+
+        for (int index = 0; index < longestCommonPrefix; index++)
+        {
+            returnPrefix += firstWord[index];
+        }
+        
+        return returnPrefix;
+    }
+    
+}
+
---
+++ UnitTests/MediumPuzzleTests.cs
@@ -0,0 +1,22 @@
+﻿
+
+public class MediumPuzzleTests
+{
+    [Fact]
+    public void LongestCommonPrefix_FL()
+    {
+        string[] strings = ["flower", "flow", "flight"];
+        var returnValue = MediumPuzzles.LongestCommonPrefix(strings);
+        Assert.Equal(returnValue, "fl");
+    }
+    
+    [Fact]
+    public void LongestCommonPrefix_Empty()
+    {
+        string[] strings = ["dog","racecar","car"];
+        var returnValue = MediumPuzzles.LongestCommonPrefix(strings);
+        
+        Assert.Equal(returnValue, "");
+    }
+    
+}
